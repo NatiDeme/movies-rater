b066aa39449c2ad9e1f6c8e63fe08490ae4a1fa5
Felix Ouma <fatonoh@gmail.com>
2022-06-21 15:28:33 +0300

    Display movie list on the homepage

--- src/modules/movies.js
+++ src/modules/movies.js
@@ -5,19 +5,39 @@ class Movies {
   }
 
   getMovieList = async (key) => {
-    let Query = `search/shows?q=${key}`;
+    const Query = `search/shows?q=${key}`;
     const list = await fetch(`${this.API_BASE_URL}${Query}`).then((response) => response.json());
     this.movies = [...this.movies, ...list];
   }
 
   displayMovies = async () => {
-    await this.getMovieList('action');
-    await this.getMovieList('comedy');
-    
+    await this.getMovieList('girls');
+    await this.getMovieList('boys');
+    await this.getMovieList('cats');
+
+    const list = this.movies.reduce((prev, curr) => {
+      if (curr.show.image) {
+        prev += `
+        <div class="movie-item mb-1">
+          <div>
+            <div class="d-flex space-around mb-1">
+             <img src=${curr.show.image.medium} />
+            </div>
+            <div class="d-flex movie-content mb-1 space-evenly">
+              <span>${curr.show.name}</span>
+              <span> <i class="fa fa-heart" data-id=${curr.show.id}></i> 0Like(s)</span>
+            </div>
+            <div class="d-flex space-around mb-1">
+              <button class="btn" data-id=${curr.show.id}>Comments</button>
+            </div>
+          </div>
+        </div>
+        `;
+      }
+      return prev;
+    }, '');
+    document.querySelector('.movies-list').innerHTML = list;
   }
-
-
-
 }
 
 export default Movies;
