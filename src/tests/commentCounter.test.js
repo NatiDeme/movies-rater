93c29811a577d72443478121c579b3008ec95602
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-06-22 16:55:45 +0300

    Add comment counter with jest test using mock API call

--- src/tests/commentCounter.test.js
+++ src/tests/commentCounter.test.js
@@ -4,9 +4,29 @@
 
 import commentCounter from '../modules/commentCounter.js';
 
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      creation_date: '2022-06-22',
+      comment: 'Girls movie',
+      username: 'Nati',
+    },
+    {
+      creation_date: '2022-06-22',
+      comment: 'Girls movie',
+      username: 'Nati',
+    },
+    {
+      creation_date: '2022-06-22',
+      comment: 'Girls movie',
+      username: 'Nati',
+    },
+  ]),
+}));
+
 describe('test comments calculation function', () => {
   test('test number of comment', async () => {
     const sum = await commentCounter(139);
-    expect(sum).toBe(2);
+    expect(sum).toBe(3);
   });
 });
